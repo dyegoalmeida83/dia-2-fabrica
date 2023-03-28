5d93b229bf9e45a97883c6a0626bf7b1ce9226eb
dyegoalmeida83 <almeida.uni1@gmail.com>
2023-03-28 17:59:09 -0300

    consumindo api

---
+++ script.js
@@ -0,0 +1,15 @@
+function inserirTituloNaDiv() {
+    const meuLocal = document.getElementById ('nomeDaFabrica')
+    const meulocal2 = document.getElementById ('nomeDaFabrica2')
+
+    const titulo = document.createElement ('h1')
+    titulo.innerText = 'consegui'
+    meuLocal.appendChild(titulo)
+
+    // 1 - criar local para criar o elemento
+    // 2 - pegar onde vamos criar o elemento    
+    // 3 - criar o meu elemento
+    // 4 - adicionar o elemento criado 
+
+
+}
